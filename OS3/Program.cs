9a3ddda1ef2784ef841beda3686fb09cbd4de018
rapogahu <sushi.yowiro@gmail.com>
2022-04-06 22:56:46 +0300

    UPDATED !!!

--- OS3/Program.cs
+++ OS3/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Concurrent;
-
-class Consumer
+﻿class Consumer
 {
     Thread thread;
 
@@ -15,15 +12,16 @@ class Consumer
     {
     Consuming:
         {
-            if (Program.itemQueue.Count == 0)
+            lock (Program.itemQueue)
             {
-                goto Consuming;
-            }
-            else
-            {
-                lock (Program.itemQueue)
+                if (Program.itemQueue.Count == 0)
+                {
+                    goto Consuming;
+                }
+                else
                 {
                     Program.itemQueue.Dequeue();
+                    goto Consuming;
                 }
             }
         }
@@ -43,37 +41,31 @@ class Producer
     {
     Producing:
         {
-            if (Program.itemQueue.Count >= 100)
+            lock (Program.itemQueue)
             {
-                goto Producing;
-            }
-            else
-            {
-                lock (Program.itemQueue)
+                if (Program.itemQueue.Count >= 100)
+                {
+                    goto Producing;
+                }
+                else
                 {
                     Random rnd = new Random();
                     int s = rnd.Next(0, 100);
 
                     Program.itemQueue.Enqueue(s);
+                    goto Producing;
                 }
             }
-        } 
+        }
     }
 }
 class Program
 {
     static public Queue<int> itemQueue = new Queue<int>();
 
-    static public void Main()
+    static void printQueue()
     {
-        Producer producer1 = new Producer();
-        Producer producer2 = new Producer();
-        Producer producer3 = new Producer();
-
-        Consumer consumer1 = new Consumer();
-        Consumer consumer2 = new Consumer();
-
-        while (true)
+    Printing:
         {
             lock (itemQueue)
             {
@@ -85,7 +77,25 @@ class Program
                 Console.WriteLine();
                 Console.WriteLine("\n\n q - остановить производителей");
             }
+            Thread.Sleep(500);
+            goto Printing;
+        }
+    }
 
+    static public void Main()
+    {
+        Producer producer1 = new Producer();
+        Producer producer2 = new Producer();
+        Producer producer3 = new Producer();
+
+        Consumer consumer1 = new Consumer();
+        Consumer consumer2 = new Consumer();
+
+        Thread printThread = new Thread(printQueue);
+        printThread.Start();
+
+        while (true)
+        {
             ConsoleKey key = Console.ReadKey().Key;
             if (key == ConsoleKey.Q)
             {
